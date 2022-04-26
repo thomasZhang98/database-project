1de320e9ace3f91951d8a2d9f994bb2716140c40
thomasZhang98 <zhangjiayang@northeastern.edu>
2022-04-26 11:19:25 -0400

    stuff

--- covid.py
+++ covid.py
@@ -8,12 +8,17 @@ userDataTable = 'user_data'
 testSubjectType = 'test_subject'
 centerEmployeeType = 'center_employee'
 labEmployeeType = 'lab_employee'
+loggedInId = -1
+userType = None
+subType = None
+
 testingSubjectInstructions = '''Please select from the following options:
 a. Schedule an appointment
 b. View tests
-c. Delete a test
+c. Unschedule a scheduled test
 d. View personal information
 e. Update personal information
+f. exit
 '''
 
 # Test status: 'PENDING' -> 'SENT TO LAB' -> 'POSITIVE/NEGATIVE'
@@ -32,10 +37,9 @@ if __name__ == '__main__':
     # Query if they want to sign up or log in.
     while True:
         loginOrSignup = input('Do you want to log in or sign up? a. "Log in" b. "Sign up": ')
-        if loginOrSignup not in ['a', 'b']:
-            print('Invalid value, please try again:')
-        else:
+        if loginOrSignup in ['a', 'b']:
             break
+        print('Invalid value, please try again:')
 
     # Retrieve username, password and role
     logged = False
@@ -62,26 +66,23 @@ if __name__ == '__main__':
             # Check if the username is in the table
             try:
                 cur = connection.cursor()
-                # TODO: Use procedure/function to verify if a user_name exists instead
-                stmt_select = 'SELECT user_name FROM user_data'
-                cur.execute(stmt_select)
-                rows = cur.fetchall()
-                if {'user_name': username} not in rows:
-                    print('Username not found, please try again.')
-                else:
+                stmt_check_if_username_exists = 'CALL check_if_username_exists(' + username + ')'
+                cur.execute(stmt_check_if_username_exists)
+                found = cur.fetchone()
+                if found:
                     try:
                         # Log in the system by getting the 'user_id' if the 'user_name' is found.
                         cur2 = connection.cursor()
-                        # TODO: Use procedure/function to login and get user_id instead
-                        stmt_select2 = 'SELECT user_id FROM user_data WHERE user_name=\'' + username + '\' and password=\'' + password + '\' and user_type=\'' + userType + '\''
-                        cur2.execute(stmt_select2)
-                        uid = cur2.fetchone()['user_id']
-                        print('uid: ' + str(uid))
-                        break
+                        stmt_getUserId = 'CALL getUserId(\'' + username + '\', \'' + password + '\', \'' + userType + '\')'
+                        cur2.execute(stmt_getUserId)
+                        loggedInId = cur2.fetchone()['user_id']
+                        print(username + ' logged in')
                     except pymysql.err.OperationalError as e:
                         print('Error: %d: %s' % (e.args[0], e.args[1]))
                         print('Please try again:')
                     cur2.close()
+                else:
+                    'Username not found, please try again:'
             except pymysql.err.OperationalError as e:
                 print('Error: %d: %s' % (e.args[0], e.args[1]))
                 print('Please try again:')
@@ -89,26 +90,136 @@ if __name__ == '__main__':
 
         # Otherwise if the operation is signup, call the procedure to add a user in the 'user_data' table        
         else:
-            # Check if the username is in the table
             try:
                 cur = connection.cursor()
-                # TODO: Use procedure/function to verify if a user_name exists instead
-                stmt_select = 'SELECT user_name FROM user_data'
-                cur.execute(stmt_select)
-                rows = cur.fetchall()
-                if {'user_name': username} in rows:
-                    print('Username already taken, please choose another username.')
-                else:
-                    try:
-                        # Add new records to the user_data table.
-                        cur2 = connection.cursor()
-                        # TODO: Use procedure/function to login and get user_id instead
-                        
-                        break
-                    except pymysql.err.OperationalError as e:
-                        print('Error: %d: %s' % (e.args[0], e.args[1]))
-                        print('Please try again:')
-                    cur2.close()
+                stmt_check_if_username_exists = 'CALL check_if_username_exists(' + username + ')'
+                cur.execute(stmt_check_if_username_exists)
+                found = cur.fetchone()
+                # Sign up a test subject
+                elif userType == testSubjectType:
+                    while True:
+                        subtype = input('What kind of testing subject is this? a. Student b. Professor c. Staff')
+                        if subtype in ['a', 'b', 'c']:
+                            break
+                        print('Invalid value, please try again:')
+                    
+                    full_name = input('What is the full name?')
+                    birthdate = input('What is the birthdate?')
+                    gender = input('What is the gender?')
+                    phone_number = input('What is the 10-digit phone number?')
+                    email = input('What is the email?')
+                    address = input('What is the address?')
+                    test_frequency = input('What is the test frequency?')
+                    if subtype == 'a':
+                        try:
+                            college = input('What is the college?')
+                            major = input('What is the major?')
+                            year_graduate = int(input('What is the year of graduation?'))
+                            # Add new records to the student table.
+                            cur2 = connection.cursor()
+                            stmt_insert_student = 'CALL insert_student(\'' + full_name + '\', \'' 
+                                                                           + birthdate + '\', \'' 
+                                                                           + gender '\', \'' 
+                                                                           + phone_number + '\', \'' 
+                                                                           + email + '\', \''
+                                                                           + address + '\', \''
+                                                                           + test_frequency + '\', \''
+                                                                           + college + '\', \''
+                                                                           + major + '\', \''
+                                                                           + year_graduate + '\', \''
+                                                                           + '\')'
+                            cur2.execute(stmt_insert_student)
+                            loggedInId = cur2.fetchone()['user_id']
+                            break
+                        except pymysql.err.OperationalError as e:
+                            print('Error: %d: %s' % (e.args[0], e.args[1]))
+                            print('Please try again:')
+                    elif subtype == 'b':
+                        try:
+                            college = input('What is the college?')
+                            department = input('What is the department?')
+                            # Add new records to the professor table.
+                            cur2 = connection.cursor()
+                            stmt_insert_professor = 'CALL insert_professor(\'' + full_name + '\', \'' 
+                                                                                + birthdate + '\', \'' 
+                                                                                + gender '\', \'' 
+                                                                                + phone_number + '\', \'' 
+                                                                                + email + '\', \''
+                                                                                + address + '\', \''
+                                                                                + test_frequency + '\', \''
+                                                                                + college + '\', \''
+                                                                                + department '\')'
+                            cur2.execute(stmt_insert_professor)
+                            loggedInId = cur2.fetchone()['user_id']
+                            break
+                        except pymysql.err.OperationalError as e:
+                            print('Error: %d: %s' % (e.args[0], e.args[1]))
+                            print('Please try again:')
+                    else:
+                        try:
+                            department = input('What is the department?')
+                            job_title = input('What is the job title?')
+                            # Add new records to the staff table.
+                            cur2 = connection.cursor()
+                            stmt_insert_staff = 'CALL insert_staff(\'' + full_name + '\', \'' 
+                                                                           + birthdate + '\', \'' 
+                                                                           + gender '\', \'' 
+                                                                           + phone_number + '\', \'' 
+                                                                           + email + '\', \''
+                                                                           + address + '\', \''
+                                                                           + test_frequency + '\', \''
+                                                                           + department + '\', \''
+                                                                           + job_title '\')'
+                            cur2.execute(stmt_insert_staff)
+                            loggedInId = cur2.fetchone()['user_id']
+                            break
+                        except pymysql.err.OperationalError as e:
+                            print('Error: %d: %s' % (e.args[0], e.args[1]))
+                            print('Please try again:')
+
+                # Sign up a testing center employee
+                elif userType == centerEmployeeType:
+                    while True:
+                        first_name = input('What is the first name?')
+                        last_name = input('What is the last name?')
+                        start_date = input('What is the start date? Use format of yyyy-mm-dd')
+                        center_id = input('What is the center id?')
+                        phone_number = int(input('What is the phone number?'))
+                        email = input('What is the email?')
+                        try:
+                            # Add new records to the center_employee table.
+                            cur2 = connection.cursor()
+                            stmt_insert_center_employee = 'CALL insert_center_employee(\'' + first_name + ' ' + last_name + '\', \'' + start_date + '\', \'' center_id '\', \'' + phone_number + '\', \'' + email + '\')'
+                            cur2.execute(stmt_insert_center_employee)
+                            loggedInId = cur2.fetchone()['user_id']
+                            break
+                        except pymysql.err.OperationalError as e:
+                            print('Error: %d: %s' % (e.args[0], e.args[1]))
+                            print('Please try again:')
+                        cur2.close()
+
+                # Sign up a lab employee type
+                elif userType == labEmployeeType:
+                    while True:
+                        first_name = input('What is the first name?')
+                        last_name = input('What is the last name?')
+                        start_date = input('What is the start date? Use format of yyyy-mm-dd')
+                        phone_number = int(input('What is the phone number?'))
+                        email = input('What is the email?')
+                        try:
+                            # Add new records to the lab_employee table.
+                            cur2 = connection.cursor()
+                            stmt_insert_lab_employee = 'CALL insert_lab_employee(\'' + first_name + ' ' + last_name + '\', \'' + start_date + '\', \'' + phone_number + '\', \'' + email + '\')'
+                            cur2.execute(stmt_insert_lab_employee)
+                            loggedInId = cur2.fetchone()['user_id']
+                            break
+                        except pymysql.err.OperationalError as e:
+                            print('Error: %d: %s' % (e.args[0], e.args[1]))
+                            print('Please try again:')
+                        cur2.close()
+                
+                print('Account succesfully signed up and logged in!')
+                break
             except pymysql.err.OperationalError as e:
                 print('Error: %d: %s' % (e.args[0], e.args[1]))
                 print('Please try again:')
@@ -116,13 +227,95 @@ if __name__ == '__main__':
 
     # Testing Subject
     # CREATE: N/A
-    # DELETE: They can delete schduled tests from the test table.
+    # DELETE: N/A
     # READ: They can read their personal information and look at their own tests and results.
-    # UPDATE: They can update their personal information.
+    # UPDATE: They can update their personal information. Update test status from 'scheduled' to 'cancelled'
     if role == 'a':
         while True:
-            option = input(testingSubjectInstructions)
-            print(option)
+            while True:
+                option = input(testingSubjectInstructions)
+                if option in ['a', 'b', 'c', 'd', 'e', 'f']:
+                    break
+                print('Invalid value, please try again:')
+
+            # Schedule an appointment
+            if option == 'a':
+                test_date = input('Which date do you want to get tested? Answer in the format of yyyy-mm-dd')
+                try:
+                    # Add a new test to the table
+                    cur2 = connection.cursor()
+                    stmt_add_test = 'CALL add_test(\'' + user_id + '\', \'' + test_date + '\')'
+                    cur2.execute(stmt_add_test)
+                    print('Test successfully scheduled!')
+                except pymysql.err.OperationalError as e:
+                    print('Error: %d: %s' % (e.args[0], e.args[1]))
+                    print('Please try again:')
+                cur2.close()
+
+
+            # View tests
+            elif option == 'b':
+                try:
+                    # Retrieve tests for the logged in account
+                    cur2 = connection.cursor()
+                    stmt_get_tests_for_user = 'CALL get_tests(\'' + loggedInId + '\')'
+                    cur2.execute(stmt_get_tests_for_user)
+                    tests = cur2.fetchall()
+                    for test in tests:
+                        print(test)
+                except pymysql.err.OperationalError as e:
+                    print('Error: %d: %s' % (e.args[0], e.args[1]))
+                    print('Please try again:')
+                cur2.close()
+
+            # Unschedule a scheduled test
+            elif option == 'c':
+                test_id_to_be_unscheduled = input('Which test id do you want to unschedule?')
+                try:
+                    # Unschedule scheduled tests for the logged in account
+                    cur2 = connection.cursor()
+                    stmt_unschdule_test = 'CALL unschedule_test(\'' + test_id_to_be_unscheduled + '\')'
+                    cur2.execute(stmt_unschdule_test)
+                    print('Test successfully unscheduled')
+                except pymysql.err.OperationalError as e:
+                    print('Error: %d: %s' % (e.args[0], e.args[1]))
+                    print('Please try again:')
+                cur2.close()
+
+            # View personal information
+            elif option == 'd':
+                try:
+                    # Retrieve the account info for the current logged in account
+                    cur2 = connection.cursor()
+                    stmt_get_account_info = 'CALL get_account_info(\'' + loggedInId + '\')'
+                    cur2.execute(stmt_get_account_info)
+                    account_info = cur2.fetchall()
+                    for info in account_info:
+                        print(info)
+                except pymysql.err.OperationalError as e:
+                    print('Error: %d: %s' % (e.args[0], e.args[1]))
+                    print('Please try again:')
+                cur2.close()
+
+            # Update personal information
+            elif option == 'e':
+                field = input('Which field do you want to modify?')
+                newValue = input('What value do you want to ')
+                try:
+                    # Update the field of the logged in account to the new value
+                    cur2 = connection.cursor()
+                    stmt_update_account_info = 'CALL update_account_info(\'' + loggedInId + '\', \'' + field + '\', \'' + newValue + '\')'
+                    cur2.execute(stmt_update_account_info)
+                    print('Field ' + field + 'updated!')
+                except pymysql.err.OperationalError as e:
+                    print('Error: %d: %s' % (e.args[0], e.args[1]))
+                    print('Please try again:')
+                cur2.close()
+
+            # Exit the program
+            elif option == 'f':
+                print('Logged out!')
+                break
 
     # Testing Center Employee
     # CREATE: They can create tests for testing subjects with initial status of 'PENDING'
